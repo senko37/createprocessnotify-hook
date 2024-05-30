fcc31c61b2b0b665fed2a842edad760702cd7884
snk37 <122232821+snk37@users.noreply.github.com>
2024-05-30 11:52:05 +0400

    Code refactoring

--- defines.h
+++ defines.h
@@ -1,5 +1,32 @@
 #pragma once
 
+// https://doxygen.reactos.org/dc/d8d/struct__EX__CALLBACK__ROUTINE__BLOCK.html
+typedef struct _EX_CALLBACK_ROUTINE_BLOCK {
+	EX_RUNDOWN_REF RundownProtect;
+	PEX_CALLBACK_FUNCTION Function;
+	PVOID Context;
+} EX_CALLBACK_ROUTINE_BLOCK, * PEX_CALLBACK_ROUTINE_BLOCK;
+
+// https://doxygen.reactos.org/da/dde/struct__EX__FAST__REF.html
+typedef struct _EX_FAST_REF {
+	union {
+		PVOID Object;
+		ULONG_PTR RefCnt : 3;
+		ULONG_PTR Value;
+	};
+} EX_FAST_REF, *PEX_FAST_REF;
+
+// https://doxygen.reactos.org/de/db2/struct__EX__CALLBACK.html
+typedef struct _EX_CALLBACK {
+	EX_FAST_REF RoutineBlock;
+} EX_CALLBACK, *PEX_CALLBACK;
+
+// https://doxygen.reactos.org/d1/d6e/ntoskrnl_2ex_2callback_8c.html#a7f04776f362c6cb892759f0200bfde86
+typedef PEX_CALLBACK_ROUTINE_BLOCK(__fastcall* tExReferenceCallBackBlock)(PEX_CALLBACK);
+
+// https://doxygen.reactos.org/d1/d6e/ntoskrnl_2ex_2callback_8c.html#ae57f266eb4bb9028bb4bcacc2588cb4a
+typedef void(__fastcall* tExDereferenceCallBackBlock)(PEX_CALLBACK, PEX_CALLBACK_ROUTINE_BLOCK);
+
 enum PROCESS_NOTIFY_TYPE : UINT32 {
 	PsSetCreateProcessNotifyRoutine_Default = 0,
 	PsSetCreateProcessNotifyRoutine_Remove,
--- main.cpp
+++ main.cpp
@@ -4,13 +4,10 @@
 PDRIVER_OBJECT GDriverObject;
 VOID DriverUnload(PDRIVER_OBJECT DriverObject);
 
-typedef EX_RUNDOWN_REF* (__fastcall* tExReferenceCallBackBlock)(void*);
 tExReferenceCallBackBlock ExReferenceCallBackBlock;
-
-typedef void(__fastcall* tExDereferenceCallBackBlock)(void*, EX_RUNDOWN_REF*);
 tExDereferenceCallBackBlock ExDereferenceCallBackBlock;
 
-RTL_RUN_ONCE* PspCreateProcessNotifyRoutine;
+PEX_CALLBACK PspCreateProcessNotifyRoutine;
 
 PLDR_DATA_TABLE_ENTRY FindKernelModuleByName(PUNICODE_STRING ModuleName) {
 	PLDR_DATA_TABLE_ENTRY Entry = (PLDR_DATA_TABLE_ENTRY)GDriverObject->DriverSection;
@@ -19,7 +16,7 @@ PLDR_DATA_TABLE_ENTRY FindKernelModuleByName(PUNICODE_STRING ModuleName) {
 			return Entry;
 		Entry = (PLDR_DATA_TABLE_ENTRY)Entry->InLoadOrderLinks.Flink;
 	}
-	return 0;
+	return NULL;
 }
 
 PLDR_DATA_TABLE_ENTRY FindKernelModuleByAddress(PVOID Address) {
@@ -29,83 +26,81 @@ PLDR_DATA_TABLE_ENTRY FindKernelModuleByAddress(PVOID Address) {
 			return Entry;
 		Entry = (PLDR_DATA_TABLE_ENTRY)Entry->InLoadOrderLinks.Flink;
 	}
-	return 0;
+	return NULL;
 }
 
-void* SignatureScan(void* Memory, size_t Limit, const char* Signature, const char* Mask) {
-	size_t SignatureSize = strlen(Signature);
-	if (SignatureSize > strlen(Mask))
-		return 0;
-	for (int i = 0; i < Limit; i++) {
-		for (int a = 0; a < SignatureSize; a++) {
-			if (((char*)Memory)[i + a] == Signature[a] || Mask[a] == '?') {
-				if (a == (SignatureSize - 1))
-					return (void*)((UINT64)Memory + i);
+PVOID SignatureScan(PVOID Mem, UINT32 Limit, CONST CHAR* Pattern, CONST CHAR* Mask, UINT32 Size) {
+	for (UINT32 i = 0; i < Limit - Size; i++) {
+		for (UINT32 a = 0; a < Size; a++) {
+			if (Mask[a] == '?' || ((CHAR*)Mem)[i + a] == Pattern[a]) {
+				if (a == Size - 1)
+					return &((CHAR*)Mem)[i];
 			}
 			else
 				break;
 		}
 	}
-	return 0;
+
+	return NULL;
 }
 
-bool InitFunctions(PLDR_DATA_TABLE_ENTRY Ntoskrnl) {
+BOOLEAN InitFunctions(PLDR_DATA_TABLE_ENTRY Ntoskrnl) {
 	ExReferenceCallBackBlock = (tExReferenceCallBackBlock)SignatureScan(Ntoskrnl->DllBase, Ntoskrnl->SizeOfImage,
-		"\x48\x89\x5C\x24\x10\x57\x48\x83\xEC\x20\x48\x8B\xF9\x0F\x0D\x09\x48\x8B\x19\xF6\xC3\x0F", "xxxxxxxxxxxxxxxxxxxxxx");
+		"\x48\x89\x5C\x24\x10\x57\x48\x83\xEC\x20\x48\x8B\xF9\x0F\x0D\x09\x48\x8B\x19\xF6\xC3\x0F", "xxxxxxxxxxxxxxxxxxxxxx", 22);
 	if (!ExReferenceCallBackBlock)
-		return false;
+		return FALSE;
 
 	ExDereferenceCallBackBlock = (tExDereferenceCallBackBlock)SignatureScan(Ntoskrnl->DllBase, Ntoskrnl->SizeOfImage,
-		"\x48\x83\xEC\x28\x4C\x8B\xCC\x0F\x0D\x09\x48\x8B\x01\x4C\x8B\xC0\x4C\x33\xC2\x49\x83\xF8\x0F", "xxxxxx?xxxxxxxxxxxxxxxx");
+		"\x48\x83\xEC\x28\x4C\x8B\xCC\x0F\x0D\x09\x48\x8B\x01\x4C\x8B\xC0\x4C\x33\xC2\x49\x83\xF8\x0F", "xxxxxx?xxxxxxxxxxxxxxxx", 23);
 	if (!ExDereferenceCallBackBlock)
-		return false;
+		return FALSE;
 
-	PspCreateProcessNotifyRoutine = (RTL_RUN_ONCE*)SignatureScan(Ntoskrnl->DllBase, Ntoskrnl->SizeOfImage,
-		"\x4C\x8D\x2D\xCC\xCC\xCC\xCC\x48\x8D\x0C\xDD\x00\x00\x00\x00", "xxx????xxxxxxxx");
+	PspCreateProcessNotifyRoutine = (PEX_CALLBACK)SignatureScan(Ntoskrnl->DllBase, Ntoskrnl->SizeOfImage,
+		"\x4C\x8D\x2D\xCC\xCC\xCC\xCC\x48\x8D\x0C\xDD\x00\x00\x00\x00", "xxx????xxxxxxxx", 15);
 	if (!PspCreateProcessNotifyRoutine)
-		return false;
+		return FALSE;
 
-	PspCreateProcessNotifyRoutine = (RTL_RUN_ONCE*)((UINT64)PspCreateProcessNotifyRoutine +
+	PspCreateProcessNotifyRoutine = (PEX_CALLBACK)((UINT64)PspCreateProcessNotifyRoutine +
 		*(UINT32*)((UINT64)PspCreateProcessNotifyRoutine + 0x3) + 0x7);
 
 	if (!MmIsAddressValid(PspCreateProcessNotifyRoutine))
-		return false;
+		return FALSE;
 
-	return true;
+	return TRUE;
 }
 
-bool SetupCreateProcessNotifyHook(PUNICODE_STRING ModuleName, PROCESS_NOTIFY_TYPE NotifyType,
+BOOLEAN SetupCreateProcessNotifyHook(PUNICODE_STRING ModuleName, PROCESS_NOTIFY_TYPE NotifyType,
 	PVOID Hook, PVOID& Original)
 {
 	for (int i = 0; i < 0x40; i++) {
-		EX_RUNDOWN_REF* CurrentCallback = ExReferenceCallBackBlock(&PspCreateProcessNotifyRoutine[i]);
+		PEX_CALLBACK_ROUTINE_BLOCK CurrentCallback = ExReferenceCallBackBlock(&PspCreateProcessNotifyRoutine[i]);
 		if (!CurrentCallback)
 			continue;
 
-		UINT32 CallbackType = *(UINT32*)((UINT64)CurrentCallback + 0x10);
-		PVOID* CallbackAddress = (PVOID*)((UINT64)CurrentCallback + 0x8);
-		if (CallbackType != NotifyType || !MmIsAddressValid(*CallbackAddress))
+		UINT32 CallbackType = (UINT32)CurrentCallback->Context;
+		if (CallbackType != NotifyType || !MmIsAddressValid(CurrentCallback->Function))
 			goto DereferenceCallBackBlock;
 
-		PLDR_DATA_TABLE_ENTRY Module = FindKernelModuleByAddress(*CallbackAddress);
+		PLDR_DATA_TABLE_ENTRY Module = FindKernelModuleByAddress(CurrentCallback->Function);
 		if (!Module || RtlCompareUnicodeString(ModuleName, &Module->BaseDllName, true) != 0)
 			goto DereferenceCallBackBlock;
 
-		DbgPrint("Callback function: %llx (Type: %i) (%ws)\n", (UINT64)*CallbackAddress, CallbackType, Module->BaseDllName.Buffer);
+		DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL,
+			"Callback function: 0x%llX (Type: %i) (%ws)\n", CurrentCallback->Function, CallbackType, Module->BaseDllName.Buffer);
 
-		Original = *CallbackAddress;
-		*CallbackAddress = Hook;
+		Original = CurrentCallback->Function;
+		CurrentCallback->Function = (PEX_CALLBACK_FUNCTION)Hook;
 
 	DereferenceCallBackBlock:
 		ExDereferenceCallBackBlock(&PspCreateProcessNotifyRoutine[i], CurrentCallback);
-		if (*CallbackAddress == Hook)
-			return true;
+		if (CurrentCallback->Function == Hook)
+			return TRUE;
 	}
-	return false;
+	return FALSE;
 }
 
 PCREATE_PROCESS_NOTIFY_ROUTINE_EX CreateProcessNotifyOriginal;
-void CreateProcessNotifyHook(PEPROCESS Process, HANDLE ProcessId, PPS_CREATE_NOTIFY_INFO CreateInfo) {
+VOID CreateProcessNotifyHook(PEPROCESS Process, HANDLE ProcessId, PPS_CREATE_NOTIFY_INFO CreateInfo) {
 	if (ProcessId == (HANDLE)0x1337)
 		return;
 
